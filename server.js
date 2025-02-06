67a033df259083b749054c4b2043119ceeb82cb2
MattADodd <121894934+MattADodd@users.noreply.github.com>
2025-02-05 16:48:17 -0800

    Started server 2 back end

---
+++ server.js
@@ -0,0 +1,55 @@
+const http = require("http");
+const port = 3000;
+const urlModule = require("url");
+const dictionary = []
+let count = 0;
+
+class dictionaryListing {
+    constructor(word, definition) {
+        this.word = word;
+        this.definition = definition;
+    }
+}
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = urlModule.parse(req.url, true);
+    const url = parsedUrl.pathname.replace(/\/$/, "");
+    const query = parsedUrl.query;
+
+    if (req.method === POST) {
+        let body = "";
+        count += 1;
+
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        const parsedBody = body.replace(/([^:]*:)(.*)/, "$1|$2").split("|");
+        dictionary.push(new dictionaryListing(parsedBody[0], parsedBody[1]));
+
+        req.on('end', () => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Data received', body: body }));
+        });
+    } else if (req.method === GET) {
+        let body = "";
+        count += 1;
+
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        let dListing;
+        for (listing in dictionary) {
+            if (listing.word === body) {
+                dListing = listing;
+                break;
+            }
+        }
+
+        req.on('end', () => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ dListing }));
+        });
+    }
+});
